418e1e1773dfebe5cbffcb90906037fdc6c2d1b5
Verma Ritu <ritu.verma2@soprasteria.com>
2022-11-01 16:04:52 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CurrConvertor';
+  amount:number=0;
+
+  public dropDownValue = "";
+  SetDropDownValue(drpValue : any){
+    this.dropDownValue = drpValue.target.value;
+  }
 }
